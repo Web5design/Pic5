e277d6f748b9408b5317648925ac8c61c0ee9b80
Sam Dutton <samdutton@gmail.com>
2012-05-16 16:19:58 +0100

    First commit

---
+++ Documents/devToolsIO/slide_config.js
@@ -0,0 +1,34 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Chrome Dev Tools',
+    subtitle: 'Evolutions',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1',
+    favIcon: 'http://bleedinghtml5.appspot.com/images/chrome-logo-tiny2.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Sam Dutton',
+    company: 'Developer Advocate, Google Chrome',
+    gplus: 'plus.samdutton.com',
+    twitter: '@sw12',
+    www: 'http://www.samdutton.com'
+  }, {
+    name: 'Pavel Feldman',
+    gplus: 'https://plus.sandbox.google.com/110463377488142794847',
+    company: 'Software Engineer, Google Chrome',
+    twitter: '@repenaxa'/*,
+    www: 'http://www.ericbidelman.com'*/
+  }]
+};
+
